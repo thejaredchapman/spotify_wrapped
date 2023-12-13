c18cfad9128c21007f0d3251cfd0a578df0f4019
thejaredchapman <thejaredchapman@gmail.com>
2023-12-13 16:30:07 -0600

    remove unnecessary imports, write clearer instructions

--- spotify_wrapped.py
+++ spotify_wrapped.py
@@ -1,16 +1,13 @@
-import requests
 import spotipy 
 from spotipy.oauth2 import SpotifyOAuth
-import pandas as pd
-import time
-import datetime
+
 
 #TODO(Change parameter to match information from SPOTIFY WEB API, CLIENT_ID, CLIENT_SECRET, REDIRECT_URL, SCOPE)
 sp =spotipy.Spotify(auth_manager=SpotifyOAuth(CLIENT_ID =CLIENT_ID
                                                 CLIENT_SECRET=CLIENT_SECRET
                                                 REDIRECT_URL=REDIRECT_URL
                                                 SCOPE=SCOPE))
-
+#TODO(Currently set to Mariah Carey, from spotify web browser, go to artist page then copy and paste the last alphanumeric query)
 mariah_uri = 'spotify:artist:6HL4z0CvFAxyc27GXpf02'
 
 results = sp.artist_albums(mariah_uri, album_type='album')
