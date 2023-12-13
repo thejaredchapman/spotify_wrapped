1dbf9f692de57d6dbecaa55a93b5c8c89beb2698
thejaredchapman <thejaredchapman@gmail.com>
2023-12-12 20:27:49 -0600

    add artists uri and results view

--- spotify_wrapped.py
+++ spotify_wrapped.py
@@ -10,3 +10,14 @@ sp =spotipy.Spotify(auth_manager=SpotifyOAuth(CLIENT_ID =CLIENT_ID
                                                 CLIENT_SECRET=CLIENT_SECRET
                                                 REDIRECT_URL=REDIRECT_URL
                                                 SCOPE=SCOPE))
+
+mariah_uri = 'spotify:artist:6HL4z0CvFAxyc27GXpf02'
+
+results = sp.artist_albums(mariah_uri_uri, album_type='album')
+albums = results['items']
+while results['next']:
+    results = sp.next(results)
+    albums.extend(results['items'])
+
+for album in albums:
+    print(album['name'])
