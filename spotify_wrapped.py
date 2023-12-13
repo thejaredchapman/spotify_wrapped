8fa500e29179957c842d4f2f88338f379c2cae5d
thejaredchapman <thejaredchapman@gmail.com>
2023-12-13 16:31:33 -0600

    update parameters for SpotifyOAuth to open browser

--- spotify_wrapped.py
+++ spotify_wrapped.py
@@ -3,10 +3,11 @@ from spotipy.oauth2 import SpotifyOAuth
 
 
 #TODO(Change parameter to match information from SPOTIFY WEB API, CLIENT_ID, CLIENT_SECRET, REDIRECT_URL, SCOPE)
-sp =spotipy.Spotify(auth_manager=SpotifyOAuth(CLIENT_ID =CLIENT_ID
-                                                CLIENT_SECRET=CLIENT_SECRET
-                                                REDIRECT_URL=REDIRECT_URL
-                                                SCOPE=SCOPE))
+sp =spotipy.Spotify(auth_manager=SpotifyOAuth(CLIENT_ID =CLIENT_ID,
+                                                CLIENT_SECRET=CLIENT_SECRET,
+                                                REDIRECT_URL=REDIRECT_URL,
+                                                SCOPE=SCOPE,
+                                                OPEN_BROWSER=True))
 #TODO(Currently set to Mariah Carey, from spotify web browser, go to artist page then copy and paste the last alphanumeric query)
 mariah_uri = 'spotify:artist:6HL4z0CvFAxyc27GXpf02'
 
